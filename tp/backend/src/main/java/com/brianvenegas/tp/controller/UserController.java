ce9a3161feee00cb45bcccb24a8c7374a38033bf
bbvenega <bbvenega@asu.edu>
2024-06-26 17:51:02 -0700

    Progress On BE

---
+++ tp/backend/src/main/java/com/brianvenegas/tp/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.brianvenegas.tp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brianvenegas.tp.model.User;
+import com.brianvenegas.tp.service.UserService;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
---
+++ tp/backend/src/main/java/com/brianvenegas/tp/model/Attractions.java
@@ -0,0 +1,56 @@
+package com.brianvenegas.tp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Attractions {
+
+    @Id
+    private Long rideId;
+    private String rideName;
+    private int standByTime;
+    private boolean status;
+
+    public Attractions() {
+    }
+
+    public Attractions(Long rideId, String rideName, int standByTime, boolean status) {
+        this.rideId = rideId;
+        this.rideName = rideName;
+        this.standByTime = standByTime;
+        this.status = status;
+    }
+
+    public Long getRideId() {
+        return rideId;
+    }
+
+    public void setRideId(Long newRideId) {
+        this.rideId = newRideId;
+    }
+
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String newRideName) {
+        this.rideName = newRideName;
+    }
+
+    public int getStandByTime() {
+        return standByTime;
+    }
+
+    public void setStandByTime(int newStandByTime) {
+        this.standByTime = newStandByTime;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean newStatus) {
+        this.status = newStatus;
+    }
+}
