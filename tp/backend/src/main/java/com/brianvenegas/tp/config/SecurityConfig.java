b5d18fd9d2f0b3c23a49fdf583a10c6462217321
bbvenega <bbvenega@asu.edu>
2024-07-06 23:06:33 -0700

    gitignore

--- tp/backend/src/main/java/com/brianvenegas/tp/config/SecurityConfig.java
+++ tp/backend/src/main/java/com/brianvenegas/tp/config/SecurityConfig.java
@@ -1,9 +1,25 @@
 package com.brianvenegas.tp.config;
 
-import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    
+
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(withDefaults());
+        return http.build();
+    }
 }
