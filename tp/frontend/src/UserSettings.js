78c0743edcd22165353c49182255cf370b2f3b84
bbvenega <bbvenega@asu.edu>
2024-07-08 00:06:00 -0700

    Got REACT REACTING MAN

---
+++ tp/frontend/src/UserSettings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserSettings.css';
+
+const UserSettings = () => {
+    return (
+        <div>
+            <h1>User Settings</h1>
+        </div>
+    );
+};
+
+export default UserSettings;
