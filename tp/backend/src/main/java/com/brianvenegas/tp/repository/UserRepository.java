c09ab390107d6a144d8dd09293283c5d623c7e27
bbvenega <bbvenega@asu.edu>
2024-07-08 23:18:30 -0700

    Sort of got Auth0 working??

--- tp/backend/src/main/java/com/brianvenegas/tp/repository/UserRepository.java
+++ tp/backend/src/main/java/com/brianvenegas/tp/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.brianvenegas.tp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import com.brianvenegas.tp.model.User;
 @Repository
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByAuth0Id(String email);
 }
---
+++ tp/frontend/src/LandingPage.js
@@ -0,0 +1,13 @@
+// LandingPage.js
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h2>Welcome to My App</h2>
+            <p>Please log in to access more features.</p>
+        </div>
+    );
+};
+
+export default LandingPage;
