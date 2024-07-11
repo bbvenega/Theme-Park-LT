8ee70e139028b8ef56eab4321e0e1b005d482a02
bbvenega <bbvenega@asu.edu>
2024-07-10 17:52:47 -0700

    Got it to a callback mismatch???

--- tp/backend/src/main/java/com/brianvenegas/tp/config/SecurityConfig.java
+++ tp/backend/src/main/java/com/brianvenegas/tp/config/SecurityConfig.java
@@ -1,26 +1,35 @@
 package com.brianvenegas.tp.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests
+                        -> authorizeRequests
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer(server -> server
+                .jwt(jwt -> jwt.decoder(jwtDecoder())));
+
+        return http.build();
+    }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/api/public").permitAll()
-            .anyRequest().authenticated()
-            )
-            .oauth2Login(withDefaults());
-        return http.build();
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://dev-iowg40sbidzmwlif.us.auth0.com/.well-known/jwks.json").build();
     }
 }
