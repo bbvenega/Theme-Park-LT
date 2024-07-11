65c53d5b5becf6919da3438368b3d41357562dcc
bbvenega <bbvenega@asu.edu>
2024-07-11 16:01:32 -0700

    Working On Comms

--- tp/backend/src/main/java/com/brianvenegas/tp/config/SecurityConfig.java
+++ tp/backend/src/main/java/com/brianvenegas/tp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,15 +16,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests
-                        -> authorizeRequests
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(withDefaults())
-                .oauth2ResourceServer(server -> server
-                .jwt(jwt -> jwt.decoder(jwtDecoder())));
+http
+    .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+        .requestMatchers("/public/**").permitAll()
+        .anyRequest().authenticated())
+    .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
 
         return http.build();
     }
