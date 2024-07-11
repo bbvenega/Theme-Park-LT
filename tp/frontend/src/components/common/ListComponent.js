51d92cb3cb27523f41b78b1ba11bbe0c02576760
bbvenega <bbvenega@asu.edu>
2024-07-11 01:24:38 -0700

    Broken Hook

--- tp/frontend/src/components/common/ListComponent.js
+++ tp/frontend/src/components/common/ListComponent.js
@@ -4,29 +4,44 @@ import axios from 'axios';
 
 const ListComponent = () => {
     const {isAuthenticated, getAccessTokenSilently} = useAuth0();
-    const [list, setList] = useState([]);
-
+    const [parks, setParks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             if (isAuthenticated) {
                 try {
                     const token = await getAccessTokenSilently();
-                    const response = await axios.get('http://localhost:8080/api/list', {
+                    console.log('Fetched token', token);
+                    const response = await axios.get('/api/parks', {
                         headers: {
                             Authorization: `Bearer ${token}`,
                         },
                     });
-
-                    setList(response.data);
+                    console.log('Fetched parks data:', response.data);
+                    setParks(response.data);
+                    setLoading(false);
                 } catch (error) {
-                    console.error("Error fetching the list", error);
+                    console.error('Error fetching data:', error);
+                    setError(error);
+                    setLoading(false);
                 }
             }
         };
 
         fetchData();
     }, [isAuthenticated, getAccessTokenSilently]);
+
+    console.log('ListComponent rendered. isAuthenticated:', isAuthenticated);
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+    if(error) {
+        return <div>Error: {error.message}</div>
+    }
 
     if (!isAuthenticated) {
         return <div>Please Log In to view your list!</div>;
@@ -36,10 +51,12 @@ const ListComponent = () => {
         <div> 
             <h1> Theme Parks</h1>
             <ul>
-                {list.map((item) => (
-                    <li key={item.id}>{item.name}</li>
+                {parks.map((park) => (
+                    <li key={park.id}>{park.name}</li>
                 ))}
             </ul>
         </div>
     );
 };
+
+export default ListComponent;
