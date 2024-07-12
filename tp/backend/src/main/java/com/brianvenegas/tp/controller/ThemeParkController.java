1735f8613cc48d9bf23f9f5f863679976de61b84
bbvenega <bbvenega@asu.edu>
2024-07-11 23:06:47 -0700

    Working on more communicaton

--- tp/backend/src/main/java/com/brianvenegas/tp/controller/ThemeParkController.java
+++ tp/backend/src/main/java/com/brianvenegas/tp/controller/ThemeParkController.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.brianvenegas.tp.model.Attraction;
 import com.brianvenegas.tp.model.Park;
 import com.brianvenegas.tp.service.ThemeParkService;
+
 
 @RestController
 public class ThemeParkController {
@@ -22,9 +21,6 @@ public class ThemeParkController {
         return themeParkService.getParks();
     }
 
-    @GetMapping("/parks/{parkId}/attractions")
-    public List<Attraction> getAttractions(@PathVariable String parkId) {
-        return themeParkService.getAttractions(parkId);
-    }
+
 
 }
