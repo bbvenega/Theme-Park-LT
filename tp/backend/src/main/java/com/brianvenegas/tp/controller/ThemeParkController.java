82915b58c114e93caf0d25e1f7f8cc783ddc3c75
bbvenega <bbvenega@asu.edu>
2024-07-11 22:37:38 -0700

    Now loads park names and lists them
    *Communication between backend and frontend seems to be working for now.

--- tp/backend/src/main/java/com/brianvenegas/tp/controller/ThemeParkController.java
+++ tp/backend/src/main/java/com/brianvenegas/tp/controller/ThemeParkController.java
@@ -3,7 +3,6 @@ package com.brianvenegas.tp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import com.brianvenegas.tp.model.Park;
 import com.brianvenegas.tp.service.ThemeParkService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class ThemeParkController {
 
     @Autowired
