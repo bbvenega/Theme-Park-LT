fd987c620d9a8f9a58f533949f7039929caa20e8
bbvenega <bbvenega@asu.edu>
2024-08-04 21:43:02 -0700

    Updates

--- tp/backend/src/main/java/com/brianvenegas/tp/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.brianvenegas.tp.exception;
-
-// Custom exception for handling cases where a user is not found
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
